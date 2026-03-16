6ecacda292a525af89dbdcf2e5e270fd1701d77e
Douglas Katzman <dougk@google.com>
2026-03-16 10:39:23 -0400

    Force copy+resize with mark-region-gc

--- tests/stablehash.pure.lisp
+++ tests/stablehash.pure.lisp
@@ -1,8 +1,19 @@
+#+mark-region-gc (setf (extern-alien "force_compaction" (boolean 8)) t
+                       (extern-alien "minimum_compact_gen" char) 0)
+
 (defstruct s1 a)
 (defstruct s2 a b)
 
-(defvar *obj1* (make-s1))
-(defvar *obj2* (make-s2))
+(defun waste-space-and-call (f &aux result)
+  ;; use up some of the page before and after the structure under test
+  (dotimes (i 10) (funcall f))
+  (setq result (funcall f))
+  (sb-vm::close-thread-alloc-region)
+  result)
+
+(defvar *obj1* (waste-space-and-call 'make-s1))
+(defvar *obj2* (waste-space-and-call 'make-s2))
+
 (defvar *addr1* (sb-kernel:get-lisp-obj-address *obj1*))
 (defvar *addr2* (sb-kernel:get-lisp-obj-address *obj2*))
 (defvar *len1* (sb-kernel:%instance-length *obj1*))
@@ -12,9 +23,8 @@
 (defvar *primsize1* (primitive-object-size *obj1*))
 (defvar *primsize2* (primitive-object-size *obj2*))
 
-(with-test (:name :lazy-hash-slot-instance-length-invariant
-                  :skipped-on :mark-region-gc) ; nothing to test really
-  (gc)
+(with-test (:name :lazy-hash-slot-instance-length-invariant)
+  (gc :gen 2)
   ;; both should have moved
   (assert (/= (sb-kernel:get-lisp-obj-address *obj1*) *addr1*))
   (assert (/= (sb-kernel:get-lisp-obj-address *obj2*) *addr2*))
