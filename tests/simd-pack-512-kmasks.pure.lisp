f15eb95908bd5a67eb8d3bbfcb38b38f18bfdf9c
Stas Boukarev <stassats@gmail.com>
2026-08-25 20:01:45 +0300

    Don't save avx512 registers in alloc tramps
    Declare the allocation routines with :save-p :avx512, to save only the
    live registers around the call (which shouldn't happen in optimized
    code).

--- tests/simd-pack-512-kmasks.pure.lisp
+++ tests/simd-pack-512-kmasks.pure.lisp
@@ -264,41 +264,6 @@
     (assert (not (search "ALLOC" text)))
     (assert (not (search "KMOVQ" text)))))
 
-(with-test (:name :destroyed-c-registers-include-kmasks)
-  (let* ((vm (find-package "SB-VM"))
-         (fun (and vm (find-symbol "DESTROYED-C-REGISTERS" vm)))
-         (forms (and fun (funcall fun))))
-    (when (and fun forms)
-      (flet ((mask-temp-p (form)
-               (and (listp form)
-                    (eq (first form) :temporary)
-                    (let ((spec (second form)))
-                      (and (listp spec)
-                           (eq (getf (cdr spec) :sc) 'mask-reg))))))
-        (let ((mask-temps (remove-if-not #'mask-temp-p forms)))
-          (assert (= (length mask-temps) 7))
-          (assert
-           (equal
-            (sort (mapcar (lambda (form)
-                            (getf (cdr (second form)) :offset))
-                          mask-temps)
-                  #'<)
-            '(1 2 3 4 5 6 7))))))))
-
-(with-test (:name :avx512-state-tn-p)
-  (let* ((vm (find-package "SB-VM"))
-         (c (find-package "SB-C"))
-         (pred (and vm (find-symbol "AVX512-STATE-TN-P" vm)))
-         (mask-reg (and vm (find-symbol "MASK-REG" vm)))
-         (sc-or-lose (and c (find-symbol "SC-OR-LOSE" c)))
-         (make-random-tn (and c (find-symbol "MAKE-RANDOM-TN" c)))
-         (sc (and sc-or-lose mask-reg
-                  (funcall sc-or-lose mask-reg)))
-         (tn (and make-random-tn sc
-                  (funcall make-random-tn sc 0))))
-    (when (and pred tn)
-      (assert (funcall pred tn)))))
-
 (with-test (:name :kandq-disassembly)
   (let* ((fun (compile nil
     '(lambda (x y)
